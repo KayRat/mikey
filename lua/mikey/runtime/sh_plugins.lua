f73a15d401decca3f607d951f420aa0256f67cb3
KayRat <kyle.ratti@outlook.com>
2015-09-08 22:30:26 -0400

    Added plugin networking system
    Very early alpha

--- lua/mikey/runtime/sh_plugins.lua
+++ lua/mikey/runtime/sh_plugins.lua
@@ -1,6 +1,24 @@
 mike = mike or {}
 mike.plugins = mike.plugins or {}
 mike.plugins.list = mike.plugins.list or {}
+mike.plugins.netMessages = mike.plugins.netMessages or {}
+
+util.AddNetworkString("mike.plugins.netMessage")
+
+net.Receive("mike.plugins.netMessage", function(len, pl)
+  local msgName = net.ReadString()
+
+  if(not IsValid(mike.plugins.netMessages[msgName])) then
+    mike.log.error("Tried to handle net message '"..msgName.."' without handler")
+    return
+  end
+
+  local data = net.ReadTable()
+  data["sender"] = pl
+
+  mike.log.info("Handling net message '"..msgName.."'"..(SERVER and " from "..pl:Nick()))
+  mike.plugins.netMessages[msgName](data)
+end)
 
 function mike.plugins.new(strName)
     local tblSkeleton = {
@@ -15,7 +33,30 @@ function mike.plugins.new(strName)
             return IsValid(objPl)
         end,
         ["onRun"] = function(self, objPl, strFirst, tblArgs) end,
+
+        ["sendNetMessage"] = function(self, target, data)
+          net.Start("mike.plugins.netMessage")
+            net.WriteString(self:getName())
+            net.WriteTable(SERVER and data or target)
+
+          if(SERVER) then
+            net.Send(target)
+          else
+            net.SendToServer()
+          end
+        end,
     }
+
+    tblSkeleton["handleNetMessage"] = function(self, msgName, callback)
+      mike.plugins.netMessages[self:getName()] = mike.plugins.netMessages[self:getName()] or {}
+      mike.plugins.netMessages[self:getName()][msgName] = callback
+    end
+
+    tblSkeleton["removeNetMessage"] = function(self, msgName)
+      if(mike.plugins.netMessages[self:getName()]) then
+        mike.plugins.netMessages[self:getName()][msgName] = nil
+      end
+    end
 
     tblSkeleton.__index = tblSkeleton
 
@@ -31,14 +72,4 @@ end
 
 function mike.plugins.add(objPlugin)
     mike.plugins.list[objPlugin:getName()] = objPlugin
-
-    if(not SERVER) then return end
-
-    local objCmd = mike.commands.get(objPlugin:getCommand())
-
-    if(objCmd) then
-        mike.commands.list[objCmd:getCommand()].canUserRun = objPlugin.canUserRun
-    else
-        mike.log.error("No command for plugin found")
-    end
 end
