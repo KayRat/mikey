f95974626a54d6a1b02147465871f3da2e4f8b05
KayRat <kyle.ratti@outlook.com>
2015-09-26 19:31:07 -0400

    Refactored bootstrapper to share any file without a specific realm in its name
    Also completely ignores files starting with _

--- lua/autorun/sh_mikey_bootstrap.lua
+++ lua/autorun/sh_mikey_bootstrap.lua
@@ -1,26 +1,26 @@
 local function loadDirectory(strDir)
-    local tblFiles,tblFolders = file.Find("mikey/"..strDir.."/*", "LUA")
+  local tblFiles,tblFolders = file.Find("mikey/"..strDir.."/*", "LUA")
 
-    for k,v in pairs(tblFiles) do
-        local strPrefix = string.sub(v, 1, 3)
+  for k,v in pairs(tblFiles) do
+    local strPrefix = string.sub(v, 1, 3)
 
-        if(strPrefix == "sh_") then
-            include("mikey/"..strDir.."/"..v)
-            AddCSLuaFile("mikey/"..strDir.."/"..v)
-        elseif(strPrefix == "cl_") then
-            if(CLIENT) then
-                include("mikey/"..strDir.."/"..v)
-            end
-            
-            AddCSLuaFile("mikey/"..strDir.."/"..v)
-        elseif(SERVER and strPrefix == "sv_") then
+    if(strPrefix == "cl_") then
+        if(CLIENT) then
             include("mikey/"..strDir.."/"..v)
         end
-    end
 
-    for k,v in pairs(tblFolders) do
-        loadDirectory(strDir.."/"..v)
+        AddCSLuaFile("mikey/"..strDir.."/"..v)
+    elseif(SERVER and strPrefix == "sv_") then
+        include("mikey/"..strDir.."/"..v)
+    else
+        include("mikey/"..strDir.."/"..v)
+        AddCSLuaFile("mikey/"..strDir.."/"..v)
     end
+  end
+
+  for k,v in pairs(tblFolders) do
+    loadDirectory(strDir.."/"..v)
+  end
 end
 
 loadDirectory("runtime")
