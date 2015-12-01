9adfa1c900b4b6d4f4015f995d4a6c77afbee922
KayRat <kyle.ratti@outlook.com>
2015-12-01 14:40:49 -0500

    Added disguise feature

--- lua/vgui/msettings.lua
+++ lua/vgui/msettings.lua
@@ -3,7 +3,8 @@ local PANEL = {}
 function PANEL:Init()
   local objSettings = vgui.Create("MButton", self)
   objSettings:SetText("Settings")
-  objSettings:Dock(FILL)
+  objSettings:SetIcon("icon16/cog.png")
+  objSettings:Dock(LEFT)
   objSettings.DoClick = function(selfButton)
     local objNoFunction = vgui.Create("MModal", self:GetParent():GetParent())
     objNoFunction:SetTitle("information")
@@ -32,9 +33,33 @@ function PANEL:Init()
   self.m_Settings = objSettings
 end
 
+function PANEL:InvalidateLayout()
+  if(LocalPlayer():IsAdmin()) then
+    local objDisguise = vgui.Create("MButton", self)
+    objDisguise:SetText("Disguise")
+    objDisguise:SetIcon("icon16/status_online.png")
+    objDisguise:Dock(LEFT)
+    objDisguise:DockMargin(2, 0, 0, 0)
+
+    self.m_Disguise = objDisguise
+  else
+    if(self.m_Disguise) then
+      self.m_Disguise:Remove()
+    end
+  end
+end
+
+function PANEL:PerformLayout(iWidth, iHeight)
+  if(self.m_Disguise) then
+    local iHalfsies = iWidth/2-1
+    self.m_Settings:SetWidth(iHalfsies)
+    self.m_Disguise:SetWidth(iHalfsies)
+  else
+    self.m_Settings:SetWidth(iWidth)
+  end
+end
+
 function PANEL:Paint(iWidth, iHeight)
-  surface.SetDrawColor(0, 0, 0, 255)
-  surface.DrawOutlinedRect(0, 0, iWidth, iHeight)
 end
 
 vgui.Register("MSettings", PANEL, "DPanel")
