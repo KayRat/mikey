58f4b797ab674199d16ebad58f55e0bfcb144b4a
KayRat <kyle.ratti@outlook.com>
2015-12-03 16:53:39 -0500

    Added cl_fonts.lua

---
+++ lua/mikey/runtime/client/cl_fonts.lua
@@ -0,0 +1,17 @@
+surface.CreateFont("MText", {
+  ["font"]    = "DermaDefault",
+  ["size"]    = 17,
+  ["weight"]  = 500,
+})
+
+surface.CreateFont("MTextBold", {
+  ["font"]    = "DermaDefault",
+  ["size"]    = 17,
+  ["weight"]  = 700,
+})
+
+surface.CreateFont("MButtonDefault", {
+  ["font"]    = "DermaDefault",
+  ["size"]    = 17,
+  ["weight"]  = 500,
+})
