03ecc43d7e6abcdfee173365794b56e9c1ca59c5
Kyle <kyle.ratti@outlook.com>
2015-12-12 18:59:47 -0500

    Updated stats to use new table structure

--- lua/mikey/plugins/_stats/sv_playerdata.lua
+++ lua/mikey/plugins/_stats/sv_playerdata.lua
@@ -3,17 +3,23 @@ local PLAYER = FindMetaTable("Player")
 local function CreateStats(objPl)
   objPl.m_bCreateStats = true
 
-  dyndb.query("INSERT INTO player_stats (name, steamid, credits) VALUES('{1}', '{2}', {3})", {objPl:Nick(), objPl:SteamID(), 0}, function(tblResults)
+  dyndb.query("INSERT INTO player_data (steam64, name, firstjoin, lastjoin, credits) VALUES('{1}', '{2}', {3}, {4}, {5})", {
+    objPl:SteamID64(),
+    objPl:Nick(),
+    os.time(),
+    os.time(),
+    0,
+  }, function(tblResults)
     if(not IsValid(objPl)) then return end
 
-    objPl:LoadStats()
+    objPl:LoadPlayerData(true)
   end)
 end
 
-function PLAYER:LoadStats()
+function PLAYER:LoadPlayerData(bFirstJoin)
   if(self:IsBot()) then return end
 
-  dyndb.query("SELECT * FROM player_stats WHERE steamid = '{1}' LIMIT 1", {self:SteamID()}, function(tblResults)
+  dyndb.query("SELECT * FROM player_data WHERE steam64 = '{1}' LIMIT 1", {self:SteamID64()}, function(tblResults)
     if(not IsValid(self)) then return end
 
     if(not tblResults or table.Count(tblResults[1]["data"]) <= 0) then
@@ -28,10 +34,17 @@ function PLAYER:LoadStats()
       local iCredits = tonumber(tblData["credits"] or 0)
 
       self:SetNWVar("credits", iCredits)
+
+      local tblPlayers = table.Copy(player.GetAll())
+      table.RemoveByValue(tblPlayers, self)
+
+      mikey.network.send("player.firstjoin", tblPlayers, {
+        ["player"]    = self,
+      })
     end
   end)
 end
 
 hook.Add("PlayerInitialSpawn", "mikey.stats.createOrLoad", function(objPl)
-  objPl:LoadStats()
+  objPl:LoadPlayerData()
 end)
