223f4f3f0425004e9f05f85ce2b9c02fa3fca233
Kyle <kyle.ratti@outlook.com>
2016-03-27 21:05:02 -0400

    Added pl:hasPermission

--- lua/mikey/runtime/sh_players.lua
+++ lua/mikey/runtime/sh_players.lua
@@ -1,6 +1,15 @@
 mikey = mikey or {}
 mikey.players = mikey.players or {}
 
+local pl = FindMetaTable("Player")
+
+function pl:hasPermission(strPermission)
+  if(not self:getRank()) then return false end
+  return self:getRank():hasPermission(strPermission) or false
+end
+
+pl.HasPermission = pl.hasPermission
+
 mikey.players.getPrettyName = function(ply)
   local name = {
     team.GetColor(ply:Team()), ply:Nick().." (",
