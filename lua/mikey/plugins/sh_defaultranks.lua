a9dbfc14376c51903bde9f1b911e9add33774ca5
Kyle <kyle.ratti@outlook.com>
2016-03-27 21:06:49 -0400

    Removed default ranks

--- lua/mikey/plugins/sh_defaultranks.lua
+++
@@ -1,7 +0,0 @@
-hook.Add("mikey.ranks.load", "zzzzzz_defaultRanks", function()
-  mikey.ranks.create("Guest", 1)
-
-  mikey.ranks.create("Moderator", 8, {"Mod"})
-  mikey.ranks.create("Admin", 9)
-  mikey.ranks.create("Super Admin", 10)
-end)
