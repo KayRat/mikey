bd9da6543f0803303ad374d0ccabac1861b2249a
Kyle <kyle.ratti@outlook.com>
2016-08-07 22:18:18 -0400

    Added permission check to slay

--- lua/mikey/plugins/slay/sv_slay.lua
+++ lua/mikey/plugins/slay/sv_slay.lua
@@ -9,6 +9,8 @@ PLUGIN.logAction = function(self, objAdmin, tblTargets)
 end
 
 mikey.network.receive("slay.doSlay", function(objPl, tblData)
+  if(PLUGIN:canUserRun(objPl) ~= true) then return end
+
   if(not tblData["targets"]) then
     mike.log.error("Received instruction to slay without any targets")
     return
