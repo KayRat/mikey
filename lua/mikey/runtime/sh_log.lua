7aade208ede4c7c10fb837c680c85f465cff2029
Kyle <kyleratti@users.noreply.github.com>
2017-10-07 16:43:03 -0400

    Replaced logging with ubilog

--- lua/mikey/runtime/sh_log.lua
+++ lua/mikey/runtime/sh_log.lua
@@ -1,53 +1,20 @@
 mikey = mikey or {}
 mikey.log = mikey.log or {}
 
+local logger = ubilog.new("serverid")
+
 local function out(str)
   -- TODO: file and/or database logging
 end
 
 function mikey.log.warn(str, ...)
-    str = string.format(str, ...)
-
-    MsgC(unpack({
-        mikey.colors.outside,  "[",
-        color_white,          "mikey",
-        mikey.colors.outside,  "] ",
-        mikey.colors.warning,  "WARN: ",
-        color_white,          str.."\n",
-    }))
-
-    str = "[mikey] WARN: "..str
-    out(str)
+    logger:warn(str, ...)
 end
 
 function mikey.log.info(str, ...)
-    str = string.format(str, ...)
-
-    MsgC(unpack({
-        mikey.colors.outside,  "[",
-        color_white,          "mikey",
-        mikey.colors.outside,  "] ",
-        mikey.colors.info,     "INFO: ",
-        color_white,          str.."\n",
-    }))
-
-    str = "[mikey] INFO: "..str
-
-    out(str)
+    logger:info(str, ...)
 end
 
 function mikey.log.error(str, ...)
-    str = string.format(str, ...).."\n"
-
-    MsgC(unpack({
-        mikey.colors.outside,  "[",
-        color_white,          "mikey",
-        mikey.colors.outside,  "] ",
-        mikey.colors.error,    "ERROR: ",
-        color_white,          str,
-    }))
-
-    str = "[mikey] ERROR: "..str
-
-    out(str)
+    logger:error(str, ...)
 end
